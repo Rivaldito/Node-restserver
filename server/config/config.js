42eaadcbbcf9aee464bf332ac0f132b33835d5c7
Rivaldito <araquesebastian@gmail.com>
2019-03-05 09:50:39 -0400

    Agregada la variable de entorno Mongo_uri

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://sebastian:123456-@cluster0-c96s8.mongodb.net/test?retryWrites=true'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
